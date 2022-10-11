b4a4c4dc510aa877f6b18da9497c0d68a5cb484a
guiguig <jaidenougland@gmail.com>
2022-10-11 09:59:13 -0700

    Add project files.

---
+++ SplatoonToFile/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace FileToSplatoon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            int imageWidth = 320;
+            int imageHeight = 120;
+            var openFile = new Microsoft.Win32.OpenFileDialog();
+            openFile.FileName = "PNG Image";                    // Default file name
+            openFile.DefaultExt = ".png";                       // Default file extension
+            openFile.Filter = "PNG Image File (.png)|*.png";    // Filter files by extension
+            bool? openedFile = openFile.ShowDialog();
+
+            if (openedFile == true)
+            {
+                FileStream fileStream = new FileStream(openFile.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                PngBitmapDecoder decoder = new PngBitmapDecoder(fileStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                BitmapSource bitmapSource = decoder.Frames[0];
+                BitmapSource newImage = new TransformedBitmap(bitmapSource, new ScaleTransform(imageWidth / bitmapSource.PixelWidth, imageHeight / bitmapSource.PixelHeight));
+                
+                int stride = newImage.PixelWidth * (newImage.Format.BitsPerPixel / 8);
+                byte[] bitArray = new byte[imageWidth * imageHeight]; // Bits of file stored in bytes (1 = 11111111)
+                byte[] fileBytes = new byte[bitArray.Length / 8]; // The decoded file's bytes
+                newImage.CopyPixels(bitArray, stride, 0);
+
+                for (int bit = 0; bit < bitArray.Length; bit++)
+                {
+                    if (bitArray[bit] < 0x7F)
+                    {
+                        fileBytes[bit / 8] |= (byte)(1 << (bit % 8));
+                    }
+                }
+
+                FileStream outFile = new FileStream(openFile.FileName + ".dat", FileMode.Create);
+                outFile.Write(fileBytes);
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SplatoonToFile/README.md
@@ -0,0 +1,8 @@
+# FileToSplatoon
+A program to convert FileToSplatoon images back to files
+## How To Use
+Run the program, you should see a small window with some UI.\
+To convert an image, click the ``Open`` button and select the image.\
+Once you select the image and click "Open", your image will be converted back to it's original file.\
+(Note that the file might have extra bytes at the end. You can trim them in a hex editor, but be careful that you trim it the right amount.)\
+Optionally, you can choose to invert the file's bytes using the ``Invert`` CheckBox.
